4df3b4cc9ccadf2824183719fe174a2478a0aef2
Raúl Mauricio Uñate Castro <91748598+rmunate@users.noreply.github.com>
2022-07-08 10:21:02 -0500

    V 1.0.1

--- src/migration/create_ccol_table.php
+++ src/migration/create_ccol_table.php
@@ -1,19 +1,19 @@
 <?php
 
-use App\Clases\CalendarioColombia;
+use App\Clases\CCOL;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
-
+    /*-----------------------------------*/
     public function up(){
-        CalendarioColombia::SchemaCreate();
+        CCOL::SchemaCreate();
     }
 
-
+    /*-----------------------------------*/
     public function down(){
-        CalendarioColombia::DropIfExists();
+        CCOL::DropIfExists();
     }
 };
